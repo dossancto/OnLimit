7032e0f729d35b897644d790fc6c7a36ddba6a06
dossancto <dossantossilvalucas963@gmail.com>
2025-02-20 19:08:00 -0300

    create project

---
+++ src/OnLimit/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace OnLimit;
+
+public class Class1
+{
+
+}
---
+++ tests/OnLimit.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace OnLimit.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
